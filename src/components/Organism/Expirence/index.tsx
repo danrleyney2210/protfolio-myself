830d2db0316c2534f5b151a61706e4cb8ccb74e4
danrley <danrley.dev@gmail.com>
2022-10-31 20:27:58 -0300

    feat: created theme global

---
+++ src/components/Organism/Expirence/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import * as S from './styles'
+
+export const Expirence = () => {
+  return (
+    <div>Expirence</div>
+  )
+}
+
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,25 @@
 import React from 'react'
+import { About } from '../../components/Organism/About'
+import { Contact } from '../../components/Organism/Contact'
+import { Expirence } from '../../components/Organism/Expirence'
+import { Header } from '../../components/Organism/Header'
+import { Nav } from '../../components/Organism/Nav'
+import { Portfolio } from '../../components/Organism/Portfolio'
+import { Services } from '../../components/Organism/Services'
+import { Testimonials } from '../../components/Organism/Testimonials'
 
 export const Home = () => {
   return (
-    <div>
-      <p>dasdasdasd</p>
-      <p>dasdasdasd</p>
-      <p>dasdasdasd</p>
-      <p>dasdasdasd</p>
-      <p>dasdasdasd</p>
-      <p>dasdasdasd</p>
-      <p>dasdasdasd</p>
-      <p>dasdasdasd</p>
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Expirence />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <footer />
+    </>
   )
 }
