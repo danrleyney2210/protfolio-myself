21933e20cba91a513048db3ab630262502f6665f
danrley <danrley.dev@gmail.com>
2022-11-05 16:05:55 -0300

    feat: finished service

--- src/components/Organism/Services/index.tsx
+++ src/components/Organism/Services/index.tsx
@@ -1,8 +1,114 @@
 import React from 'react'
+import * as S from './styles'
+import { BiCheck } from 'react-icons/bi'
 
 export const Services = () => {
   return (
-    <div>Services</div>
+    <S.Wrapper id='services'>
+      <h5>What I offer</h5>
+      <h2>Services</h2>
+
+      <S.ServicesContainer className='container'>
+        <article className='service'>
+          <div className="service_head">
+            <h3>Ui/UX Design</h3>
+          </div>
+
+          <ul className='service_list'>
+            <li>
+              <BiCheck className='service_list_icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicig alit.</p>
+            </li>
+            <li>
+              <BiCheck className='service_list_icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicig alit.</p>
+            </li>
+            <li>
+              <BiCheck className='service_list_icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicig alit.</p>
+            </li>
+            <li>
+              <BiCheck className='service_list_icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicig alit.</p>
+            </li>
+            <li>
+              <BiCheck className='service_list_icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicig alit.</p>
+            </li>
+            <li>
+              <BiCheck className='service_list_icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicig alit.</p>
+            </li>
+          </ul>
+        </article>
+
+        <article className='service'>
+          <div className="service_head">
+            <h3>Content Creation</h3>
+          </div>
+
+          <ul className='service_list'>
+            <li>
+              <BiCheck className='service_list_icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicig alit.</p>
+            </li>
+            <li>
+              <BiCheck className='service_list_icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicig alit.</p>
+            </li>
+            <li>
+              <BiCheck className='service_list_icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicig alit.</p>
+            </li>
+            <li>
+              <BiCheck className='service_list_icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicig alit.</p>
+            </li>
+            <li>
+              <BiCheck className='service_list_icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicig alit.</p>
+            </li>
+            <li>
+              <BiCheck className='service_list_icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicig alit.</p>
+            </li>
+          </ul>
+        </article>
+
+        <article className='service'>
+          <div className="service_head">
+            <h3>Ui/UX Design</h3>
+          </div>
+
+          <ul className='service_list'>
+            <li>
+              <BiCheck className='service_list_icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicig alit.</p>
+            </li>
+            <li>
+              <BiCheck className='service_list_icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicig alit.</p>
+            </li>
+            <li>
+              <BiCheck className='service_list_icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicig alit.</p>
+            </li>
+            <li>
+              <BiCheck className='service_list_icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicig alit.</p>
+            </li>
+            <li>
+              <BiCheck className='service_list_icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicig alit.</p>
+            </li>
+            <li>
+              <BiCheck className='service_list_icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicig alit.</p>
+            </li>
+          </ul>
+        </article>
+      </S.ServicesContainer>
+    </S.Wrapper>
   )
 }
 
---
+++ src/components/Organism/Services/styles.ts
@@ -0,0 +1,83 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
+
+
+`
+
+export const ServicesContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 3rem;
+
+  .service {
+    background: ${ props => props.theme.color_bg_variant};
+    border-radius: 0 0 2rem 2rem;
+    border: 1px solid ${ props => props.theme.color_primary };
+    height: fit-content;
+    transition: ${ props => props.theme.transition};
+
+    &:hover {
+      background-color: transparent;
+      border-color: ${ props => props.theme.color_primary_variant};
+      cursor: default;
+    };
+
+    .service_head{
+      background-color: ${ props => props.theme.color_primary};
+      padding: 2rem;
+      border-radius: 0 0 2rem 2rem;
+      box-shadow: 0 2rem 1rem rgba(0 ,0, 0, 0.1);
+
+      h3 {
+        color: ${ props => props.theme.color_bg };
+        font-size: 1rem;
+        text-align: center;
+      }
+    }
+
+    .service_list {
+      padding: 2rem;
+
+      li {
+        display: flex;
+        gap: 1rem;
+        margin-bottom: 0.8rem;
+
+        p {
+          font-size: 0.9rem;
+        }
+
+        .service_list_icon {
+          color: ${ props => props.theme.color_primary };
+          margin-top: 2px;
+        }
+      }
+    }
+
+
+  }
+
+  @media( max-width: 1024px) {
+    grid-template-columns: 1fr 1fr;
+    gap: 2rem;
+
+    .service {
+      height: auto;
+
+    }
+  }
+
+  @media( max-width: 600px) {
+    grid-template-columns: 1fr;
+    gap: 2rem;
+
+    .service {
+      height: auto;
+      
+    }
+  }
+
+ 
+`
+
