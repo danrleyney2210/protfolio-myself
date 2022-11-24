6d0c53ca39d19d4d82a826cbac4ac9c4859d6487
danrley <danrley.dev@gmail.com>
2022-11-24 15:36:40 -0300

    feat: add comentario

--- src/components/Organism/Testimonials/index.tsx
+++ src/components/Organism/Testimonials/index.tsx
@@ -8,7 +8,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-import AVATAR1 from "../../../assets/avatar1.jpg";
+import AVATAR1 from "../../../assets/mardoque.jpeg";
 import AVATAR2 from "../../../assets/avatar2.jpg";
 import AVATAR3 from "../../../assets/avatar3.jpg";
 import AVATAR4 from "../../../assets/avatar4.jpg";
@@ -23,11 +23,11 @@ interface IDataProps {
 const data: IDataProps[] = [
   {
     avatar: AVATAR1,
-    name: "Heleno da Silva Souza",
+    name: "Mardoqueu Sousa",
     occupation:
       "Fullstack Software Engineer | Spring Boot | API REST | Hibernate | Nexus | Java | SQL",
     review:
-      "Extremamente apaixonado pelo que faz, muito atento aos detalhes e competente. Além das suas habilidades e experiências curriculares, ele constrói confiança nas suas relações profissionais e conduz suas atribuições com total maestria e sempre em busca de inovações.. Leva a informação de forma clara e eficiente a todos os públicos e níveis da organização. É sempre um prazer para mim dividir um convívio profissional com a Mardoqueu.",
+      "Conheci Danrley no instituto federal, ele possui uma habilidade de adaptação impar em aprender novas tecnologias de programação como também é um excelente docente. Sempre foi bastante proativo como também possui intensa paixão pelo campo de tecnologia, costumava ficar até tarde no trabalho para bater metas e os nossos coordenadores nunca duvidavam de sua capacidade profissional e garra pela área de tecnologia da informação.",
   },
   {
     avatar: AVATAR2,
